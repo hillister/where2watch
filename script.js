1d07798ac4f595e8638f6efc893b9906dd4edc18
Ridhcheall Doherty <132768875+hillister@users.noreply.github.com>
2025-04-14 15:10:26 +0100

    fetch and display  data from reverse geocoding api

--- script.js
+++ script.js
@@ -5,3 +5,22 @@ navigator.geolocation.getCurrentPosition(position => {
 }, error => {
     console.error('Location access denied', error)
 });
+
+
+
+
+async function getCountryCode(){
+    const apiKey = 'pk.3351a15efd4d628c08b6f7611d957d77';
+
+    try {
+        const apiLink = await fetch(`https://us1.locationiq.com/v1/reverse?key=${apiKey}&lat=51.50344025&lon=-0.12770820958562096&format=json&`)
+        const apiData = await apiLink.json()
+        console.log(apiData)
+        console.log(apiData.address.country_code)
+    }catch(err) {
+        console.error('Something went wrong:', err)
+    }
+
+}
+
+getCountryCode()
