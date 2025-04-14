e72c4d94a07f0783513e4dfca9ac26e1f294843d
Ridhcheall Doherty <132768875+hillister@users.noreply.github.com>
2025-04-14 15:40:11 +0100

    changed code to promises

--- script.js
+++ script.js
@@ -1,39 +1,35 @@
-/*navigator.geolocation.getCurrentPosition(position => {
-    const lat = position.coords.latitude;
-    const lon = position.coords.longitude
-
-    async function getCountryCode(){
-        const apiKey = 'pk.3351a15efd4d628c08b6f7611d957d77';
-    
-        try {
-            const apiLink = await fetch(`https://us1.locationiq.com/v1/reverse?key=${apiKey}&lat=${lat}&lon=${lon}&format=json&`)
-            const apiData = await apiLink.json()
-            console.log(apiData)
-            console.log(apiData.address.country_code)
-        }catch(err) {
-            console.error('Something went wrong:', err)
-        }
-    }
-
-    getCountryCode();
-
-}, error => {
-    console.error('Location access denied', error)
-});
-
-*/
-
 function getUserLocation(){
-    new Promise((resolve, reject) => {
+    return new Promise((resolve, reject) => {
         navigator.geolocation.getCurrentPosition(position => {
             const lat = position.coords.latitude;
             const lon = position.coords.longitude
-            console.log(lat,lon)
-            resolve(lat, lon);
+            resolve({lat, lon});
         },  error => {
             reject('Location access denied', error)
         })
     })
 }
 
-getUserLocation()
+async function getCountryCode(lat, lon){
+    const apiKey = 'pk.3351a15efd4d628c08b6f7611d957d77';
+
+    try {
+        const apiLink = await fetch(`https://us1.locationiq.com/v1/reverse?key=${apiKey}&lat=${lat}&lon=${lon}&format=json&`)
+        const apiData = await apiLink.json()
+        console.log(apiData)
+        console.log(apiData.address.country_code)
+    }catch(err) {
+        console.error('Something went wrong:', err)
+    }
+}
+
+async function fetchLocationAndCountry(){
+    try {
+        const {lat, lon} = await getUserLocation()
+        await getCountryCode(lat, lon);
+    } catch (err){
+        console.error(err)
+    }
+}
+
+fetchLocationAndCountry()
